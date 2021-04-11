1ef61ff136c348a4c95d5fa226d44aeb4b44f208
Teangelo Burks <burks.teangelo@gmail.com>
2021-04-11 13:06:28 -0400

    modified indexdb.js file to check if the app is offline/created saveRecord function

--- public/indexdb.js
+++ public/indexdb.js
@@ -4,12 +4,34 @@ let db;
 // Creating a request for our Database
 const request = indexedDB.open(`budget`, 1);
 
-request.onupgradeneeded = function(event) {
+request.onupgradeneeded = function (event) {
     // Creates object store called "Pending"
     db = event.target.result;
-    db.createObjectStore("pending", {autoIncrement: ture});
+    db.createObjectStore(`pending`, { autoIncrement: ture });
 };
 
-request.onsuccess = function(event) {
+request.onsuccess = function (event) {
     db = event.target.result;
-}
+
+    // Checks if the app is online before reading from db
+    if (navigator.onLine) {
+        checkDatabase();
+    }
+};
+request.onerror = function (event) {
+    console.log(`Oops${event.target.errorcode}`);
+};
+
+function saveRecord(record) {
+    // create a transaction on the pending db with readwrite access
+    const transaction = db.transaction(["pending"], "readwrite");
+
+    // access the pending object store
+    const store = transaction.objectStore("pending");
+
+    // using add method to add a record to the store
+    store.add(record);
+};
+
+
+
